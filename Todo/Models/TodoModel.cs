8c4e09b26d312e836c45c8a9b25ed6de82f9113c
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-20 20:03:54 +0300

    Create TodoModel

---
+++ Todo/Models/TodoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Todo.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public required string Text { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public bool IsFavorite { get; set; } = false;
+    }
+}
