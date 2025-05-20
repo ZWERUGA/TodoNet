73ee30663e830a9d61a1bccf222327992d6588d4
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-20 20:05:55 +0300

    Add ToTodoDtoFromTodoModel and ToTodoModelFromCreateTodoDto in TodoMappers

---
+++ Todo/Mappers/TodoMappers.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Dtos.Todo;
+using Todo.Models;
+
+namespace Todo.Mappers
+{
+    public static class TodoMappers
+    {
+        public static TodoDto ToTodoDtoFromTodoModel(this TodoModel todoModel)
+        {
+            return new TodoDto
+            {
+                Id = todoModel.Id,
+                Title = todoModel.Title,
+                Text = todoModel.Text,
+                IsCompleted = todoModel.IsCompleted,
+                IsFavorite = todoModel.IsFavorite,
+            };
+        }
+
+        public static TodoModel ToTodoModelFromCreateTodoDto(this CreateTodoDto createTodoDto)
+        {
+            return new TodoModel
+            {
+                Title = createTodoDto.Title,
+                Text = createTodoDto.Text,
+                IsCompleted = false,
+                IsFavorite = false,
+            };
+        }
+    }
+}
