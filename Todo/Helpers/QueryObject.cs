23ca8cd3d43bfa6fe6205cbdeeed87bcb04150f3
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-23 21:16:26 +0300

    Change QueryObject bool? types

--- Todo/Helpers/QueryObject.cs
+++ Todo/Helpers/QueryObject.cs
@@ -9,7 +9,7 @@ namespace Todo.Helpers
     {
         public string? Title { get; set; }
         public string? Text { get; set; }
-        public bool IsCompleted { get; set; }
-        public bool IsFavorite { get; set; }
+        public bool? IsCompleted { get; set; }
+        public bool? IsFavorite { get; set; }
     }
 }
