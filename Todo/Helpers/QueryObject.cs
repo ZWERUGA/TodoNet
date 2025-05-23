860e91bcbf33100a017b3ec5b804f28e73660ceb
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-23 20:45:43 +0300

    Add QueryObject for filtering

---
+++ Todo/Helpers/QueryObject.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Todo.Helpers
+{
+    public class QueryObject
+    {
+        public string? Title { get; set; }
+        public string? Text { get; set; }
+        public bool IsCompleted { get; set; }
+        public bool IsFavorite { get; set; }
+    }
+}
