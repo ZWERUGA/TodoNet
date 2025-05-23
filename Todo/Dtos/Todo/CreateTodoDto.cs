0665701326caddf43fdeb176da7211db199b672a
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-23 20:37:04 +0300

    Add validation attributes for CreateTodoDto and UpdateTodoDto

--- Todo/Dtos/Todo/CreateTodoDto.cs
+++ Todo/Dtos/Todo/CreateTodoDto.cs
@@ -1,8 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.Dtos.Todo
 {
     public class CreateTodoDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Заголовок должен состоять минимум из 3 символов.")]
+        [MaxLength(30, ErrorMessage = "Заголовок должен состоять максимум из 30 символов.")]
         public required string Title { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Текст должен состоять минимум из 3 символов.")]
+        [MaxLength(200, ErrorMessage = "Текст должен состоять максимум из 200 символов.")]
         public required string Text { get; set; }
     }
 }
--- Todo/Dtos/Todo/UpdateTodoDto.cs
+++ Todo/Dtos/Todo/UpdateTodoDto.cs
@@ -1,10 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Todo.Dtos.Todo
 {
     public class UpdateTodoDto
     {
+        [Required]
+        [MinLength(3, ErrorMessage = "Заголовок должен состоять минимум из 3 символов.")]
+        [MaxLength(30, ErrorMessage = "Заголовок должен состоять максимум из 30 символов.")]
         public required string Title { get; set; }
+
+        [Required]
+        [MinLength(3, ErrorMessage = "Текст должен состоять минимум из 3 символов.")]
+        [MaxLength(200, ErrorMessage = "Текст должен состоять максимум из 200 символов.")]
         public required string Text { get; set; }
-        public required bool IsCompleted { get; set; }
-        public required bool IsFavorite { get; set; }
+
+        public bool IsCompleted { get; set; }
+        public bool IsFavorite { get; set; }
     }
 }
