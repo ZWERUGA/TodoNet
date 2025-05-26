ac77369f06b8849ab3e3f1ef86f5087ea098ddcc
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-26 19:10:44 +0300

    Override OnModelCreating and add roles in database

--- Todo/Data/TodoDbContext.cs
+++ Todo/Data/TodoDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Todo.Models;
 
@@ -6,6 +7,29 @@ namespace Todo.Data
 {
     public class TodoDbContext(DbContextOptions options) : IdentityDbContext<AppUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles =
+            [
+                new()
+                {
+                    Id = "0926f441-7f24-4401-901e-bf527771c8f2",
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                new()
+                {
+                    Id = "0d10bdb5-82e5-4740-9ea1-dfa5b0f21275",
+                    Name = "User",
+                    NormalizedName = "USER",
+                },
+            ];
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+
         public DbSet<TodoModel> Todos { get; set; }
     }
 }
