f2aa09324f0714e50997af21df73282083c1bbb7
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-26 19:11:24 +0300

    Add SeedRole migration and update database

---
+++ Todo/Migrations/20250526160616_SeedRole.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Todo.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "0926f441-7f24-4401-901e-bf527771c8f2", null, "Admin", "ADMIN" },
+                    { "0d10bdb5-82e5-4740-9ea1-dfa5b0f21275", null, "User", "USER" },
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0926f441-7f24-4401-901e-bf527771c8f2"
+            );
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "0d10bdb5-82e5-4740-9ea1-dfa5b0f21275"
+            );
+        }
+    }
+}
