c01137ebf1ce68651ad071dd3761993d689556b0
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-29 21:53:40 +0300

    Add AppUserId and AppUser props

--- Todo/Models/Todo.cs
+++ Todo/Models/Todo.cs
@@ -7,5 +7,7 @@
         public required string Text { get; set; }
         public bool IsCompleted { get; set; } = false;
         public bool IsFavorite { get; set; } = false;
+        public string AppUserId { get; set; } = string.Empty;
+        public AppUser AppUser { get; set; } = null!;
     }
 }
