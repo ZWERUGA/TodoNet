a51eb8fa5330a6e416df1a7472e9760af9a8bd48
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-30 22:50:47 +0300

    Modify ITodoRepository

--- Todo/Interfaces/ITodoRepository.cs
+++ Todo/Interfaces/ITodoRepository.cs
@@ -7,9 +7,9 @@ namespace Todo.Interfaces
     public interface ITodoRepository
     {
         Task<List<TodoModel>> GetAllAsync(QueryObject query, string appUserId);
-        Task<TodoModel?> GetByIdAsync(int todoId);
+        Task<TodoModel?> GetByIdAsync(int todoId, string appUserId);
         Task<TodoModel> CreateAsync(TodoModel todoModel);
-        Task<TodoModel?> UpdateAsync(int todoId, UpdateTodoDto updateTodoDto);
-        Task<TodoModel?> DeleteAsync(int todoId);
+        Task<TodoModel?> UpdateAsync(int todoId, UpdateTodoDto updateTodoDto, string appUserId);
+        Task<TodoModel?> DeleteAsync(int todoId, string appUserId);
     }
 }
