2b9267423e6a9cb7cee66115c01c404a776ee7c9
ZWERUGA <maratnasyrov97@gmail.com>
2025-05-31 21:34:51 +0300

    Create useApi hook

---
+++ frontend/src/app/hooks/useApi.ts
@@ -0,0 +1,65 @@
+import { API_ENDPOINTS } from "../endpoints";
+import CreateTodoDto from "../interfaces/createTodoDto";
+import Todo from "../interfaces/todo";
+import UpdateTodoDto from "../interfaces/updateTodoDto";
+
+export function useApi() {
+  const request = async <T>(url: string, options?: RequestInit): Promise<T> => {
+    const token = localStorage.getItem("token");
+
+    if (!token) throw new Error("Необходимо войти в систему.");
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      ...options,
+    });
+
+    if (!response.ok) throw new Error(await response.text());
+
+    return response.json();
+  };
+
+  return {
+    account: {
+      register: (data: { userName: string; email: string; password: string }) =>
+        request<{ userName: string; email: string; token: string }>(
+          API_ENDPOINTS.ACCOUNT.REGISTER,
+          {
+            method: "POST",
+            body: JSON.stringify(data),
+          }
+        ),
+
+      login: (data: { userName: string; password: string }) =>
+        request<{ userName: string; email: string; token: string }>(
+          API_ENDPOINTS.ACCOUNT.LOGIN,
+          {
+            method: "POST",
+            body: JSON.stringify(data),
+          }
+        ),
+    },
+
+    todos: {
+      getAll: () => request<Array<Todo>>(API_ENDPOINTS.TODO.GET_ALL),
+      create: (data: CreateTodoDto) =>
+        request<Todo>(API_ENDPOINTS.TODO.CREATE, {
+          method: "POST",
+          body: JSON.stringify(data),
+        }),
+      getById: (id: number) => request<Todo>(API_ENDPOINTS.TODO.GET_BY_ID(id)),
+      update: (id: number, data: UpdateTodoDto) =>
+        request<Todo>(API_ENDPOINTS.TODO.UPDATE(id), {
+          method: "PUT",
+          body: JSON.stringify(data),
+        }),
+      delete: (id: number) =>
+        request<void>(API_ENDPOINTS.TODO.DELETE(id), {
+          method: "DELETE",
+        }),
+    },
+  };
+}
