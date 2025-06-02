865d03a29c27e679fad08e8f8040ef6382adfbc6
ZWERUGA <maratnasyrov97@gmail.com>
2025-06-02 19:47:10 +0300

    Add logout method to main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,12 +3,24 @@
 import { useEffect, useState } from "react";
 import { useApi } from "./hooks/useApi";
 import Todo from "./interfaces/todo";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const api = useApi();
+  const router = useRouter();
   const [todos, setTodos] = useState<Todo[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+
+  const handleLogout = async () => {
+    try {
+      await api.account.logout();
+      router.push("/account/login");
+    } catch (error) {
+      console.error("Ошибка при выходе из системы", error);
+      router.push("/account/login");
+    }
+  };
 
   useEffect(() => {
     const fetchTodos = async () => {
@@ -40,6 +52,12 @@ export default function Home() {
           todos.map((todo) => <li key={todo.id}>{todo.text}</li>)
         )}
       </ul>
+      <button
+        onClick={handleLogout}
+        className="p-2 bg-gray-600 rounded-md cursor-pointer"
+      >
+        Выйти
+      </button>
     </div>
   );
 }
