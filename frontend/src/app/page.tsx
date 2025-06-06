298bb4ee2eb332ff778aae9fcb65646bfd5bb93f
ZWERUGA <maratnasyrov97@gmail.com>
2025-06-06 13:28:47 +0300

    Add loading statements for actions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,7 +6,7 @@ import Todo from "./interfaces/todo";
 import { useRouter } from "next/navigation";
 import { useTheme } from "./providers/theme-provider";
 import CreateTodoForm from "./components/create-form-todo";
-import { FaCheck, FaStar, FaTrash } from "react-icons/fa";
+import TodoActions from "./components/todo-actions";
 
 export default function Home() {
   const api = useApi();
@@ -17,9 +17,15 @@ export default function Home() {
 
   const { theme, toggleTheme } = useTheme();
 
+  const [loadingId, setLoadingId] = useState<number | null>(null);
+  const [loadingAction, setLoadingAction] = useState<string | null>(null);
+
   const handleToggleComplete = async (id: number) => {
     const todo = todos.find((t) => t.id === id);
     if (!todo) return;
+
+    setLoadingId(id);
+    setLoadingAction("complete");
 
     try {
       const updatedTodo = await api.todos.update(id, {
@@ -28,12 +34,18 @@ export default function Home() {
       setTodos((prev) => prev.map((t) => (t.id === id ? updatedTodo : t)));
     } catch (err) {
       console.error(`Ошибка при обновлении задачи: ${err}`);
+    } finally {
+      setLoadingId(null);
+      setLoadingAction(null);
     }
   };
 
   const handleToggleFavorite = async (id: number) => {
     const todo = todos.find((t) => t.id === id);
     if (!todo) return;
+
+    setLoadingId(id);
+    setLoadingAction("favorite");
 
     try {
       const updatedTodo = await api.todos.update(id, {
@@ -42,6 +54,9 @@ export default function Home() {
       setTodos((prev) => prev.map((t) => (t.id === id ? updatedTodo : t)));
     } catch (err) {
       console.error(`Ошибка при обновлении задачи: ${err}`);
+    } finally {
+      setLoadingId(null);
+      setLoadingAction(null);
     }
   };
 
@@ -49,11 +64,17 @@ export default function Home() {
     const todo = todos.find((t) => t.id === id);
     if (!todo) return;
 
+    setLoadingId(id);
+    setLoadingAction("delete");
+
     try {
       await api.todos.delete(id);
       setTodos((prev) => prev.filter((t) => t.id !== id));
     } catch (err) {
       console.error(`Ошибка при удалении задачи: ${err}`);
+    } finally {
+      setLoadingId(null);
+      setLoadingAction(null);
     }
   };
 
@@ -84,9 +105,6 @@ export default function Home() {
     fetchTodos();
   }, []);
 
-  if (loading) return <div>Загрузка данных...</div>;
-  if (error) return <div>Ошибка: {error}...</div>;
-
   return (
     <main className="min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 py-10 px-4 transition-colors">
       <div className="max-w-xl mx-auto bg-white dark:bg-gray-800 shadow-md rounded-lg p-6">
@@ -108,8 +126,10 @@ export default function Home() {
           </div>
         </div>
 
-        {loading && <p>Загрузка данных...</p>}
-        {error && <p className="text-red-400">Ошибка: {error}</p>}
+        {loading && <p className="text-center">Загрузка данных...</p>}
+        {error && (
+          <p className="text-center text-red-400">Ошибка получения данных...</p>
+        )}
 
         {!loading && (
           <CreateTodoForm
@@ -153,32 +173,14 @@ export default function Home() {
                       </p>
                     </div>
                     <div className="space-x-2">
-                      <button
-                        onClick={() => handleToggleComplete(todo.id)}
-                        className={`px-2 py-2 text-sm rounded cursor-pointer ${
-                          todo.isCompleted
-                            ? "bg-green-400 text-white hover:bg-green-500"
-                            : "bg-gray-300 text-black hover:bg-gray-400"
-                        }`}
-                      >
-                        <FaCheck />
-                      </button>
-                      <button
-                        onClick={() => handleToggleFavorite(todo.id)}
-                        className={`px-2 py-2 text-sm rounded cursor-pointer ${
-                          todo.isFavorite
-                            ? "bg-yellow-600 hover:bg-yellow-600"
-                            : "bg-gray-300 text-black hover:bg-gray-400"
-                        }`}
-                      >
-                        <FaStar />
-                      </button>
-                      <button
-                        onClick={() => handleDelete(todo.id)}
-                        className="px-2 py-2 text-sm rounded cursor-pointer bg-red-500 hover:bg-red-800"
-                      >
-                        <FaTrash />
-                      </button>
+                      <TodoActions
+                        todo={todo}
+                        loadingId={loadingId}
+                        loadingAction={loadingAction}
+                        onComplete={handleToggleComplete}
+                        onFavorite={handleToggleFavorite}
+                        onDelete={handleDelete}
+                      />
                     </div>
                   </div>
                 </li>
